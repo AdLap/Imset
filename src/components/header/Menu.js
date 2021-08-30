145e46d55bd7ab7baf572b5b45ba327761bdcdbd
Adam <adamlapacz@gmail.com>
2021-08-30 23:01:53 +0200

    header ready

--- src/components/header/Menu.js
+++ src/components/header/Menu.js
@@ -1,10 +1,16 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './Menu.scss';
 const Menu = () => {
+    const [isActive, setIsActive] = useState(false);
+
     return (
         <div className='menu'>
-            <div className='menu__burger'>
-                
+            <div className={isActive ? 'menu__burger-active' : 'menu__burger'}
+                onClick={() => setIsActive(!isActive)}
+            >
+                <span className={isActive ? 'menu__burger-active__top' : 'menu__burger__top'}>{null}</span>
+                <span className={isActive ? 'menu__burger-active__middle' : 'menu__burger__middle'}>{null}</span>
+                <span className={isActive ? 'menu__burger-active__bottom' : 'menu__burger__bottom'}>{null}</span>
             </div>
 
         </div>
--- src/components/header/Panel.js
+++ src/components/header/Panel.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhoneAlt, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import './Panel.scss';
 
 const Panel = () => {
     return (
-        <>
-        </>
+        <ul className='panel'>
+            <li className='panel__item'>
+                centrum obsługi klienta
+                <a href='#' title='CENTRUM OBSŁUGI KLIENTA'>
+                    <span className='panel__item__phone'>
+                        <span className='panel__item__icon'>
+                            <FontAwesomeIcon icon={faPhoneAlt} />
+                        </span>
+                        800 080 800
+                    </span>
+                </a>
+            </li>
+            <li className='panel__item'>
+                <a href='#' title='SZUKAJ'>
+                    <span className='panel__item__icon'>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </span>
+                    szukaj
+                </a>
+            </li>
+            <li className='panel__item'>
+                <a href='#' title='PANEL ABONENTA'>
+                    <span className='panel__item__icon'>
+                        <FontAwesomeIcon icon={faUser} />
+                    </span>
+                    strefa abonenta
+                </a>
+            </li>
+        </ul>
     );
 }
 
