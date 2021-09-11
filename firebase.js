14b9ccc97148b4b787ea5b40764049dec6a115f1
Adam <adamlapacz@gmail.com>
2021-09-11 16:42:57 +0200

    added firebase

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlsgSMCs1LWjZquwvFA9teNDxdqT8JJmA",
+  authDomain: "tv-lp-24ad7.firebaseapp.com",
+  projectId: "tv-lp-24ad7",
+  storageBucket: "tv-lp-24ad7.appspot.com",
+  messagingSenderId: "328795105493",
+  appId: "1:328795105493:web:29680ade5e770b48a39a5c"
+};
+
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
